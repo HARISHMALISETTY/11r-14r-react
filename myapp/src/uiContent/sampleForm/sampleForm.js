0829ecab0996bb2549030558a7f46d015f06351d
HARISHMALISETTY <malisettyharish@gmail.com>
2024-12-01 19:27:56 +0530

    react-9

--- myapp/src/uiContent/sampleForm/sampleForm.js
+++ myapp/src/uiContent/sampleForm/sampleForm.js
@@ -1,11 +1,36 @@
-import React,{Component} from "react"
+import React, { Component } from "react";
 
+class SampleForm extends Component {
+  constructor() {
+    super();
+    this.state = { fname: "", fullname: "", sname: "",mob:"" };
+  }
 
-class SampleForm extends Component{
+  handleChange = (event) => {
+  
+    const {name,value} = event.target;
+    this.setState({ [name]: value });    
+  };
 
-  render(){
-    return(<>helloo</>)
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ fullname: this.state.fname + this.state.sname +this.state.mob});
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="fname" onChange={this.handleChange} />
+          <input type="text" name="sname" onChange={this.handleChange} />
+          <input type="text" name="mob" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+
+        <h1>{this.state.fullname}</h1>
+      </div>
+    );
   }
 }
 
-export default SampleForm
+export default SampleForm;
